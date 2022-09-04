9c372d0825fda1ce7c053d30d486c072234a43df
AcademyZirak <AcademyZirak@gmail.com>
2022-09-05 00:36:44 +0430

    Main

--- rust_1_004_number_1/target/debug/deps/rust_1_4_number_1-02656798af126106.d
+++
@@ -1,5 +0,0 @@
-d:\Synced Folders\Code PC1 sc\Computer Programs\Rust\Rust-Tutorial--AcademyZirak--GR\rust_1_004_number_1\target\debug\deps\rust_1_4_number_1-02656798af126106.rmeta: src\main.rs
-
-d:\Synced Folders\Code PC1 sc\Computer Programs\Rust\Rust-Tutorial--AcademyZirak--GR\rust_1_004_number_1\target\debug\deps\rust_1_4_number_1-02656798af126106.d: src\main.rs
-
-src\main.rs:
--- rust_1_004_number_1/target/debug/deps/rust_1_4_number_1.d
+++
@@ -1,5 +0,0 @@
-D:\Synced Folders\Code PC1 sc\Computer Programs\Rust\Rust-Tutorial--AcademyZirak--GR\rust_1_004_number_1\target\debug\deps\rust_1_4_number_1.exe: src\main.rs
-
-D:\Synced Folders\Code PC1 sc\Computer Programs\Rust\Rust-Tutorial--AcademyZirak--GR\rust_1_004_number_1\target\debug\deps\rust_1_4_number_1.d: src\main.rs
-
-src\main.rs:
